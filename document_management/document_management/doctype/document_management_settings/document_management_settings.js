b2438a6f21343a0857404c2be7b3d8ec09780030
HoaAnDuong <an.duong@makeai.vn>
2025-05-07 09:16:20 +0700

    Fix button handling in Document Management Settings

--- document_management/document_management/doctype/document_management_settings/document_management_settings.js
+++ document_management/document_management/doctype/document_management_settings/document_management_settings.js
@@ -1,23 +1,35 @@
 frappe.ui.form.on('Document Management Settings', {
+    custom_fetch_sharepoint_ids: function(frm) {
+        frappe.call({
+            method: 'document_management.document_management.doctype.document_management_settings.document_management_settings.fetch_sharepoint_ids_from_group',
+            args: {},
+            callback: function(r) {
+                if (!r.exc) {
+                    frappe.show_alert({
+                        message: __('SharePoint IDs fetched successfully'),
+                        indicator: 'green'
+                    });
+                    frm.refresh();
+                }
+            },
+            freeze: true,
+            freeze_message: __('Fetching SharePoint IDs...')
+        });
+    },
+
     refresh: function(frm) {
         if (frm.is_new()) return;
-        
+
+        // Add the custom button (dynamic)
         frm.add_custom_button(__('Fetch SharePoint IDs'), function() {
-            frappe.call({
-                method: 'document_management.document_management.doctype.document_management_settings.document_management_settings.fetch_sharepoint_ids_from_group',
-                args: {},
-                callback: function(r) {
-                    if (!r.exc) {
-                        frappe.show_alert({
-                            message: __('SharePoint IDs fetched successfully'),
-                            indicator: 'green'
-                        });
-                        frm.refresh();
-                    }
-                },
-                freeze: true,
-                freeze_message: __('Fetching SharePoint IDs...')
-            });
+            frm.trigger('custom_fetch_sharepoint_ids');
         });
+
+        // The 'fetch_sharepoint_ids_button' (defined in JSON) will be handled by the event below
+    },
+
+    fetch_sharepoint_ids_button: function(frm) {
+        // This function will be called when the 'fetch_sharepoint_ids_button' (from JSON) is clicked
+        frm.trigger('custom_fetch_sharepoint_ids');
     }
 });
