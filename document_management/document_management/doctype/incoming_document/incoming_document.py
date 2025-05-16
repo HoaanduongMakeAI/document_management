e78356cf15714c8f62d6199682fd8f2a665f5da4
HoaAnDuong <an.duong@makeai.vn>
2025-05-16 11:36:06 +0700

    Fix ModuleNotFoundError by using standard copy module for deepcopy

--- document_management/document_management/doctype/incoming_document/incoming_document.py
+++ document_management/document_management/doctype/incoming_document/incoming_document.py
@@ -2,7 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
-import frappe.copy
+import copy
 from frappe.model.document import Document
 from frappe.utils import strip_html, get_abbr
 
@@ -18,7 +18,7 @@ class IncomingDocument(Document):
 		if self.name: # Check if it's an existing document
 			self._original_status = frappe.db.get_value("Incoming Document", self.name, "status")
 			# Store original document tasks before save
-			self._original_document_tasks = frappe.copy.deepcopy(self.document_tasks)
+			self._original_document_tasks = copy.deepcopy(self.document_tasks)
 		else: # New document
 			self._original_status = None
 			self._original_document_tasks = {}
